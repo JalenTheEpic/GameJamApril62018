9ed16dd6c4de8e765e525588b01ce17e76ad56ea
Jalen <fearthedurp@gmail.com>
2018-04-06 21:39:49 -0400

    Add mechanics

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // Smothly open a door
+    private float smooth = 2.0f;
+
+    private float DoorOpenAngle = 90.0f;
+    private float DoorCloseAngle = 0.0f;
+    public bool open;
+    public bool enter;
+
+    //Main function
+    private void Update()
+    {
+
+        if (open == true)
+        {
+            var target = Quaternion.Euler(0, DoorOpenAngle, 0);
+            // Dampen towards the target rotation
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, target, Time.deltaTime * smooth);
+        }
+
+        if (open == false)
+        {
+            var target1 = Quaternion.Euler(0, DoorCloseAngle, 0);
+            // Dampen towards the target rotation
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, target1, Time.deltaTime * smooth);
+        }
+
+        if (enter == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                open = !open;
+            }
+        }
+    }
+
+    //Activate the Main function when player is near the door
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            enter = true;
+        }
+    }
+
+    //Deactivate the Main function when player is go away from door
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            enter = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+    public enum Items { Key1, Key2 };
+
+    public Items items;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
