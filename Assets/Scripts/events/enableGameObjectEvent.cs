c8625ab990decaea74bcd7a2f963a9b38bc0ff82
Jalen <fearthedurp@gmail.com>
2018-04-08 20:07:37 -0400

    Add some feeling

---
+++ Assets/Scripts/events/enableGameObjectEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enableGameObjectEvent : MonoBehaviour {
+
+    EventBase myEvent;
+
+    [SerializeField]
+    GameObject obj;
+
+    // Use this for initialization
+    void Start()
+    {
+        myEvent = GetComponent<EventBase>();
+
+        obj.SetActive(true);
+
+        myEvent.setComplete();
+    }
+}
